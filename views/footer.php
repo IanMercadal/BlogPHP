4347ce1e0fed2a7a82f5380b294626a8fa6a69a4
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-01-09 17:20:47 +0100

    feat: :art: Creando paginación de forma parcial y terminando inicio
    Creando archivos comunes como header y footer

---
+++ views/footer.php
@@ -0,0 +1,7 @@
+    <footer>
+        <p class="copyright">Copyright &copy; 2022-2023 Ian Mercadal<p>
+    </footer>
+
+</body>
+
+</html>
--- views/index.view.php
+++ views/index.view.php
@@ -1,42 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require 'header.php' ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="<?php echo RUTA; ?>css/estilos.css">
-    <title>Blog</title>
-</head>
-
-<body>
-    <header>
-        <div class="contenedor">
-            <div class="logo izquierda">
-                <p>
-                    <a href="#">Mi primer blog</a>
-                </p>
-            </div>
-
-            <div class="derecha">
-                <form name="busqueda" class="buscar" action="<?php echo RUTA;?>/buscar.php" method="GET">
-                    <input type="text" name="busqueda" placeholder="Buscar">
-                    <button type="submit" class="icono fa fa-search"></button>
-            </form>
-
-            <nav class="menu">
-                <ul>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#">Contacto<i class="icono fa fa-envelope"></i></a></li>
-                </ul>
-            </nav>
-            </div>
+    <div class="contenedor">
+        <div class="post">
+            <article>
+                <h2 class="Titulo">Título article</h2>
+                <p class="fecha">1 de Enero de 2016</p>
+                <div lcass="thumb">
+                    <a href="#">
+                        <img src="<?php echo RUTA; ?>/imagenes/1.png" alt="">
+                    </a>
+                </div>
+                <p class="extracto">Lorem ipsum, dolor sit amet consectetur adipisicing e</p>
+                <a href="#" class="container">Continuar Leyendo</a>
+            </article>
         </div>
-    </header>
-</body>
 
-</html>
+        <div class="post">
+            <article>
+                <h2 class="Titulo">Título article</h2>
+                <p class="fecha">1 de Enero de 2022</p>
+                <div lcass="thumb">
+                    <a href="#">
+                        <img src="<?php echo RUTA; ?>/imagenes/1.png" alt="">
+                    </a>
+                </div>
+                <p class="extracto">Lorem ipsum, dolor sit amet consectetur adipisicing e</p>
+                <a href="#" class="container">Continuar Leyendo</a>
+            </article>
+        </div>
+
+        <?php require 'paginacion.php'; ?>
+    </div>
+
+    <?php require 'footer.php'; ?>
