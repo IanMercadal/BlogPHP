2c87616a700ff68050f234b50eab69aaebbc64b0
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-01-09 19:28:55 +0100

    feat: :art: Agregando página de error
    Creando página que muestra error

---
+++ error.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'admin/config.php';
+require 'views/header.php';
+?>
+
+<div class="contenedor">
+    <div class="post">
+        <article>
+            <h2 class="articulo">Error, no se ha encontrado lo deseado</h2>
+            <br>
+        </article>
+    </div>
+</div>
+
+<?php require 'views/footer.php'; ?>
--- views/single.view.php
+++ views/single.view.php
@@ -4,7 +4,7 @@
         <div class="post">
             <article>
                 <h2 class="Titulo"><?php echo $post['titulo'] ?></h2>
-                <p class="fecha"><?php echo fecha($post['fecha']) ?></p>
+                <p class="fecha"><?php echo fecha($post['fecha']); ?></p>
                 <div lcass="thumb">
                     <img src="<?php echo RUTA; ?>/imagenes/<?php echo $post['thumb'] ?>" alt="<?php echo $post['titulo'] ?>">
                 </div>
